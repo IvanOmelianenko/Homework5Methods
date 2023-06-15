054942386b9f40a07bcb741273c51a860ecebe37
ivomon <91485616+ivomon@users.noreply.github.com>
2023-06-15 19:02:40 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 //        System.out.println(getMul(a, b));
 //        System.out.println(getDiv(a, b));
 //        System.out.println(cube(a));
-
+        System.out.println("Hello");
 
 
 
